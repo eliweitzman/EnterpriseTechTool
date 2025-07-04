628e96ccc03b1563662509c249e42f4fcb69a00b
Eli Weitzman <22529819+eliweitzman@users.noreply.github.com>
2025-07-04 10:12:19 -0400

    Update copyright year in license header
    Changed the copyright year in the license header from 2024-2025 to 2025 to reflect the current year.

--- ETT.ps1
+++ ETT.ps1
@@ -21,7 +21,7 @@
 .LICENSE
     BSD 3-Clause License
 
-    Copyright (c) 2024-2025, Eli Weitzman
+    Copyright (c) 2025, Eli Weitzman
 
     Redistribution and use in source and binary forms, with or without
     modification, are permitted provided that the following conditions are met:
